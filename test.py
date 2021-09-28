15c25e448cba5e003faafdb6466cc3a5ac164002
athaleuba <91524097+athaleuba@users.noreply.github.com>
2021-09-28 14:57:18 +0200

    Update test.py
    meilleur linspace

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print('ceci est un test')
 import numpy as np
 import matplotlib.pyplot as plt
 A = np.ones((6, 7))
-X = np.linspace(-np.pi, np.pi, 7)
+X = np.linspace(-np.pi, np.pi, 9)
 Y = np.sin(X)
 plt.plot(X, Y)
 plt.show()
