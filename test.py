1fce7e6ffffe4b941e26c25293927adc61b10865
athaleuba <athanase.leuba@mines-paristech.fr>
2021-09-28 14:29:42 +0200

    2e version
    hey

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ print('ceci est un test')
 import numpy as np
 import matplotlib.pyplot as plt
 A = np.ones((6, 7))
-print(A)
-X = np.linspace(-np.pi, np.pi, 100)
+X = np.linspace(-np.pi, np.pi, 7)
 Y = np.sin(X)
 plt.plot(X, Y)
 plt.show()
+print('tudo bem')
